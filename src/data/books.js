01ca48f204f04dd171c398e4e1a29c261e6a407c
Sergio Aguado Garrido <saguado9@gmail.com>
2024-01-01 22:56:25 +0100

    initial commit

---
+++ src/data/books.js
@@ -0,0 +1,66 @@
+export const books = [
+  {
+    id: 'the-pragmatic-programmer',
+    title: 'The Pragmatic Programmer: From Journeyman to Master',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+    opinion: `Este libro es una joya para cualquier programador. Ofrece consejos prácticos y técnicas que van más allá de la codificación, abordando la mentalidad y la artesanía del desarrollo de software. Los autores, con su enfoque en la simplicidad y la elegancia, logran transmitir valiosas lecciones sobre cómo mejorar las habilidades y convertirse en un programador más competente. Altamente recomendado para aquellos que buscan crecer profesionalmente en el mundo de la programación.`,
+    author: 'Andy Hunt'
+  },
+  {
+    id: 'clean-code',
+    title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+    opinion: `Este libro es una lectura imprescindible para cualquier desarrollador comprometido con la calidad del código. Con un enfoque en la agilidad y la artesanía del software, Robert C. Martin ofrece directrices claras para escribir código limpio y eficiente. La obra destaca la importancia de la legibilidad y la mantenibilidad del código, contribuyendo a la creación de software robusto y confiable. Una referencia valiosa para mejorar las prácticas de programación.`,
+    author: 'Robert C. Martin'
+  },
+  {
+    id: 'refactoring',
+    title: 'Refactoring: Improving the Design of Existing Code',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1539022493i/42245734.jpg',
+    opinion: `Martin Fowler presenta un enfoque esencial para la mejora continua del código. Este libro proporciona técnicas prácticas de refactorización que permiten a los desarrolladores modificar y mejorar el diseño de su código de manera eficiente. La atención al detalle y los ejemplos prácticos hacen que este libro sea una herramienta valiosa para aquellos que buscan perfeccionar sus habilidades de programación y mantener un código sostenible a lo largo del tiempo.`,
+    author: 'Martin Fowler'
+  },
+  {
+    id: 'algorithms',
+    title: 'Algorithms',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1348011905i/10803540.jpg',
+    opinion: `Este libro proporciona una sólida introducción a los algoritmos, presentando conceptos de manera clara y accesible. Robert Sedgewick aborda la complejidad de manera efectiva, ofreciendo una base sólida para comprender y aplicar algoritmos en el desarrollo de software. Acompañado de ejemplos y explicaciones detalladas, es una lectura valiosa tanto para principiantes como para aquellos con experiencia en programación.`,
+    author: 'Robert Sedgewick'
+  },
+  {
+    id: 'javascript-patterns',
+    title: 'JavaScript Patterns',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1344668503i/9422683.jpg',
+    opinion: `Este libro es una guía esencial para escribir código JavaScript de calidad. Stoyan Stefanov presenta patrones de diseño y técnicas de programación que ayudan a los desarrolladores a mejorar sus habilidades y crear aplicaciones robustas. Con un enfoque en la simplicidad y la eficiencia, este libro es una referencia valiosa para aquellos que buscan escribir código JavaScript de manera efectiva.`,
+    author: 'Stoyan Stefanov'
+  },
+  {
+    id: 'the-clean-architecture',
+    title: 'Clean Architecture',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1471680093i/18043011.jpg',
+    opinion: `Este libro es una guía esencial para el diseño de software. Robert C. Martin presenta una arquitectura de software que permite a los desarrolladores crear aplicaciones robustas y sostenibles. Con un enfoque en la separación de intereses, este libro es una referencia valiosa para aquellos que buscan mejorar sus habilidades y crear aplicaciones de calidad.`,
+    author: 'Robert C. Martin'
+  },
+  {
+    id: 'code-complete',
+    title: 'Code Complete',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1355151544i/9737826.jpg',
+    opinion: `Este libro es un recurso exhaustivo que abarca todos los aspectos del desarrollo de software. Steve McConnell proporciona consejos prácticos respaldados por la investigación, ayudando a los desarrolladores a mejorar sus habilidades y adoptar prácticas efectivas. La estructura detallada del libro y su enfoque en la ingeniería de software lo convierten en una referencia valiosa para programadores de todos los niveles.`,
+    author: 'Steve McConnell'
+  },
+  {
+    id: 'the-clean-coder',
+    title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+    opinion: `Este libro es una lectura esencial para cualquier programador profesional. Robert C. Martin presenta una guía práctica para el desarrollo de software, abordando temas como la ética, la responsabilidad y la profesionalidad. Con un enfoque en la artesanía del software, este libro es una referencia valiosa para aquellos que buscan mejorar sus habilidades y convertirse en mejores programadores.`,
+    author: 'Robert C. Martin'
+  },
+  {
+    id: 'design-patterns',
+    title: 'Design Patterns: Elements of Reusable Object-Oriented Software',
+    img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1348027904i/85009.jpg',
+    opinion: `Este libro es una guía esencial para el diseño de software. Erich Gamma, Richard Helm, Ralph Johnson y John Vlissides presentan patrones de diseño que ayudan a los desarrolladores a mejorar sus habilidades y crear aplicaciones robustas. Con un enfoque en la simplicidad y la eficiencia, este libro es una referencia valiosa para aquellos que buscan escribir código de manera efectiva.`,
+    author: 'Erich Gamma'
+  }
+  
+];
